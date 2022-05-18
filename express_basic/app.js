8d6643aca8bc9535eb5fb6c99d6da952fc804692
HugoHerreraH <launchx04415@innovaccion.mx>
2022-05-18 16:16:18 -0500

    Localhost funcionando

---
+++ express_basic/app.js
@@ -0,0 +1,15 @@
+//Usando objeto expres
+const express = require('express')
+//app de Express
+const app= express()
+//Puerto en el que vamos a ver nuestra app: localhost:3000
+const port = 3000
+//path inicial, respodera a la url localhost:3000
+app.get('/', (req, res) => {
+    res.send('Hola mundo!')
+})
+
+//Con esto inicializamos la app
+app.listen(port, () => {
+    console.log(`Example app listen on port ${port}`);
+})
